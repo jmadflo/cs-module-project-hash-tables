a2030af131f6f0b5ab9bca8ee96d125c6fcca8c8
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-07-08 16:05:14 -0700

    no_dups completed

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,5 +1,16 @@
 def no_dups(s):
     # Your code here
+    repeats = {}
+    words = s.split(' ')
+    new_words = []
+    for index, word in enumerate(words):
+        # print(index, word)
+        if word not in repeats:
+            repeats[word] = True
+            new_words.append(word)
+        # print(repeats)
+    return ' '.join(new_words)
+
 
 
 
