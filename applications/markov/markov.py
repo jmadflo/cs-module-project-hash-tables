bc0e7357e757c87ebc190bcf344239da992238ce
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-07-09 15:04:10 -0700

    markov program makes 5 sentences instead of just 1 now

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -9,8 +9,8 @@ with open("input.txt") as f:
 words = words.split()
 caps = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 table = {}
-start_words = []
-stop_words = []
+start_words = set()
+stop_words = set()
 for i, word in enumerate(words[:-1]):
     if word not in table:
         table[word] = [words[i+1]]
@@ -18,22 +18,26 @@ for i, word in enumerate(words[:-1]):
         table[word].append(words[i+1])
     # check to see if word is a start word or stop word
     if (word[0] in caps) or (word[0] == '"' and word[1] in caps):
-        start_words.append(word)
+        start_words.add(word)
     if (word[-1] in '.?!') or (word[-1] == '"' and word[-2] in '.?!'):
-        stop_words.append(word)
-stop_words.append(words[-1])
+        stop_words.add(word)
+stop_words.add(words[-1])
 # print(start_words)
 # print(table)
 # TODO: construct 5 random sentences
 # Your code here
-start_word = random.choice(start_words)
-print(start_word, end=" ")
-prev = start_word
-while True:
-    next_word = random.choice(table[prev])
-    print(next_word, end=" ")
-    if next_word in stop_words:
-        break
-    else:
-        prev = next_word
-print("") # new line on console
+print("\n") # new line on console
+for x in range(5): # each iteration of the loop makes one sentence, so we run 5 interations for 5 sentences
+    start_words = list(start_words)
+    stop_words = list(stop_words)
+    start_word = random.choice(start_words)
+    print(start_word, end=" ")
+    prev = start_word
+    while True:
+        next_word = random.choice(table[prev])
+        print(next_word, end=" ")
+        if next_word in stop_words:
+            break
+        else:
+            prev = next_word
+print("\n") # new line on console
